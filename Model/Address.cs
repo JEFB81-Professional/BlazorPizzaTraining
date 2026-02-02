061b6d48eb8dafb8896e8ce2a707e8cdd0a1f781
Jhaner Fernandez <jhanerfernandez@gmail.com>
2026-02-02 11:22:52 +0100

    added submit validation but it is not working as expected

--- Model/Address.cs
+++ Model/Address.cs
@@ -6,13 +6,13 @@ namespace BlazingPizza
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "You must set a name")]
+        [Required, MinLength(3, ErrorMessage = "Please use a Name bigger than 3 letters."), MaxLength(100, ErrorMessage = "Please use a Name less than 100 letters.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "You must set a main address")]
+        [Required, MinLength(5, ErrorMessage = "Please use an Address bigger than 5 letters."), MaxLength(100, ErrorMessage = "Please use an Address less than 100 letters.")]
         [Display(Name = "Main Address")]
         public string Line1 { get; set; }
-    
+
         public string Line2 { get; set; }
         [Required(ErrorMessage = "You must set a city")]
         public string City { get; set; }
